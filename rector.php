fa6eb566b5149d8c775d9f3b14f990b13dea6d83
Labeya <laurentmwn@gmail.com>
2026-04-26 22:49:23 +0100

    Install and Setup rector

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+   ->withPaths([
+      __DIR__ . '/app',
+      __DIR__ . '/bootstrap',
+      __DIR__ . '/resources',
+      __DIR__ . '/routes',
+      __DIR__ . '/tests',
+   ])
+   // uncomment to reach your current PHP version
+   // ->withPhpSets()
+   ->withSets([
+      LaravelSetList::LARAVEL_CODE_QUALITY,
+      LaravelSetList::LARAVEL_COLLECTION,
+   ])
+   ->withTypeCoverageLevel(0)
+   ->withDeadCodeLevel(0)
+   ->withCodeQualityLevel(0);
